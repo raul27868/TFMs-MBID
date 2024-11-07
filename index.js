99ca2c4a1fe2d26384124e6978abadc84a0c8a61
Raul Reyero <34958262+raul27868@users.noreply.github.com>
2024-11-07 20:12:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+console.log('Hola')
+
+
+function displayData(data) {
+  const table = document.getElementById("data-table");
+  data.slice(1).forEach(row => {  // slice(1) omite el encabezado de la hoja
+    const newRow = table.insertRow();
+    row.forEach(cell => {
+      const newCell = newRow.insertCell();
+      newCell.textContent = cell;
+    });
+  });
+}
